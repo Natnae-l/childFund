bd1ee6fbbf02b658e4e5eec2beb8ba8c555bb131
Natnael Teklemariam <natnaelmverse1@gmail.com>
2024-01-03 17:04:03 +0300

    added a project files

---
+++ app.js
@@ -0,0 +1,50 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+// dotenv configuration
+require('dotenv').config()
+
+// initialized app
+const app = express();
+app.listen(process.env.PORT, () => console.log(`app listening on port: ${process.env.PORT}`))
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//middlewares
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error-handler/error');
+});
+
+module.exports = app;
---
+++ public/javascripts/index.js
@@ -0,0 +1,3 @@
+document.querySelector('.menu').addEventListener('click', () => {
+    document.querySelector(".ful").classList.toggle("show")
+})
