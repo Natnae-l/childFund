0080b453631d98fa7313dfadc75d55a0bba0e274
Natnael Teklemariam <natnaelmverse1@gmail.com>
2024-01-04 15:35:47 +0300

    model added

---
+++ model/user.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName:{required: true, type: String},
+    lastName:{required: true, type: String},
+    email: {required: true, type: String},
+    password: {required: true, type: String, minLength: 8}
+})
