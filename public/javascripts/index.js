0f6c8ba257eef4f8d9c785503388915843144849
Natnael Teklemariam <natnaelmverse1@gmail.com>
2024-01-10 11:32:28 +0300

    un wanted contents removed

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,9 @@
 document.querySelector('.menu').addEventListener('click', () => {
-    document.querySelector(".ful").classList.toggle("show");
-    document.querySelector(".second-nav").classList.toggle("second-width")
+    document.querySelector(".menu-lii").classList.toggle("show");
+    // document.querySelector(".second-nav").classList.toggle("second-width")
+})
+Array.from(document.querySelectorAll('.link')).forEach(item => {
+    item.addEventListener('click', () => {
+    document.querySelector(".menu-lii").classList.remove("show");
+})
 })
